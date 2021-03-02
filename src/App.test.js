3c299b952b31cddbfad5db90faf81037f3c357b9
Javel Wilson <thevagabondcode@gmail.com>
2021-03-02 14:15:57 -0500

    Create coming soon page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders coming soon text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Wah Gwan, Soon Come/i);
+  expect(h1Element).toBeInTheDocument();
 });
